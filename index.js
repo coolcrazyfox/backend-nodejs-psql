b3b6f1514edf011a6b2683b3c208230f3a1e44be
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-09-01 22:49:05 +0300

    change postcontroller

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const userRouter = require('./routes/user.routes')
+const postRouter = require('./routes/post.routes')
 
 const PORT = process.env.PORT || 8080
 
@@ -7,6 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use('/api', userRouter)
+app.use('/api', postRouter)
 // app.get('/',(req, res)=>{
 //     res.send('my text + NODEJS!!')
 // })
