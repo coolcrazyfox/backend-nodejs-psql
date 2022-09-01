2b3de610d3036e0c10db0df51ef7c2b4059ffb96
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-09-01 22:56:30 +0300

    change postcontroller

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,9 +1,14 @@
+const db = require('../db')
 class PostController{
 
     async createPost(req,res){
-
+        const {title, content, userId} = req.body
+        const newPost = await db.query(`INSERT INTO post (title, content, userId) values ($1, $2, $3) RETURNING *`, [title, content, userId])
+        res.json(newPost.rows[0])
     }
     async getPostsByUser(req,res){
+        const id = req.query.id
+
 
     }
 
