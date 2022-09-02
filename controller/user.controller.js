c5c5c83104150fb53ad55143872594c674e4bbd7
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-09-02 13:01:11 +0300

    change postController

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,34 +2,59 @@ const db =require('../db')
 
 class UserController{
     async createUser(req,res){
-        const{name,surname}=req.body
-        const newPerson =await db.query(`INSERT INTO person (name, surname) values ($1, $2) RETURNING *`, [name, surname])
-        console.log(name, surname)
-        res.json(newPerson.rows[0])
-
+        try{
+            const{name,surname}=req.body
+            const newPerson =await db.query(`INSERT INTO person (name, surname) values ($1, $2) RETURNING *`, [name, surname])
+            console.log(name, surname)
+            res.json(newPerson.rows[0])
+        }catch (e){
+            res.status(500).json(e)
+        }
     }
     async getUsers(req,res){
-        const users =await db.query('SELECT * FROM person')
-        res.json(users.rows)
-
+        try{
+            const users =await db.query('SELECT * FROM person')
+            res.json(users.rows)
+        }catch (e){
+            res.status(500).json(e)
+        }
     }
     async getOneUser(req,res){
-        const id = req.params.id
-        const user =await db.query('SELECT * FROM person WHERE id = $1', [id])
-        res.json(user.rows[0])
-    }
-    async updateUser(req,res){
-        const {id, name, surname} =req.body
-        const user = await db.query('UPDATE person SET name =$1, surname =$2 WHERE id = $3 RETURNING *',
-            [name, surname, id]
-        )
-        res.json(user.rows[0])
+        try{
+            const id = req.params.id
+            const user =await db.query('SELECT * FROM person WHERE id = $1', [id])
+            res.json(user.rows[0])
+        }catch (e){
+            res.status(500).json(e)
+        }
 
     }
+    async updateUser(req,res){
+        try{
+            const {id, name, surname} =req.body
+            if (!{id, name, surname}){
+                res.status(400).json({message:"id , name, surname aren't specified"})
+            }
+            const user = await db.query('UPDATE person SET name =$1, surname =$2 WHERE id = $3 RETURNING *',
+                [name, surname, id]
+            )
+            res.json(user.rows[0])
+        }catch (e){
+            res.status(500).json(e)
+        }
+    }
     async deleteUser(req,res){
-        const id = req.params.id
-        const user =await db.query('DELETE FROM person WHERE id = $1', [id])
-        res.json(user.rows[0])
+        try{
+            const id = req.params.id
+            if (!id){
+                res.status(400).json({message:"id , name, surname aren't specified"})
+            }
+            const user =await db.query('DELETE FROM person WHERE id = $1', [id])
+            res.json(user.rows[0])
+        }catch (e){
+            res.status(500).json(e)
+        }
+
 
     }
 }
