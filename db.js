5dfa430ec9b0af92d69138b72c7e75c1a205856c
Andrei Malets <coolviktori@yandex.by>
2022-10-29 17:18:07 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
     user:'postgres',
-    password:'man21061985xxx',
+    password:'man',
     host:'localhost',
     port:5432,
     database:'node_postgres'
